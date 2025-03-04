5235b173e67f37f8403f1a8b19471b54289c5895
MobSlicer152 <brambleclaw1414@gmail.com>
2025-03-04 14:09:16 -0500

    Add main loop and scoreboard stuff

--- main.py
+++ main.py
@@ -5,12 +5,29 @@ def main():
     state = []
     board.reset(state)
 
-    # TODO: main loop where you take input and print the board
+    # TODO: add input
+    running = True
+    winner = board.NONE
+    while running:
+        for player in [board.X, board.O]:
+            x = 0 # get the x and y on the board
+            y = 0
+            while not board.set(state, x, y, player):
+                # get position until it's an allowed one
+                pass
+            if board.check_win(player):
+                winner = player
+                break
+
+        # keep running until a tie or someone wins
+        running = not board.check_tie(state) and winner == board.NONE
 
     scoreboard = save.load(save.PATH)
 
     # TODO: update scoreboard with win/loss/tie
-    # save.add(scoreboard, winner, loser, board.check_tie(state))
+    winner_name = "" # get winner name
+    loser_name = "" # get loser name
+    save.add(scoreboard, winner_name, loser_name, board.check_tie(state))
     save.print_all(scoreboard)
 
     save.write(scoreboard, save.PATH)
